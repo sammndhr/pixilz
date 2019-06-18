df6b87cb614d05b9e9c5c1e0dd2fd37ec2ca94ec
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-06-18 12:36:14 -0700

    CSS and add sticky header

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../styles/App.scss'
+import Navigation from './Navigation.js'
+
+function App() {
+  return (
+    <div className='App container'>
+      <Navigation />
+      <main>
+        <h1>Main</h1>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+        <p>Here is the main body.</p>
+      </main>
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default App
