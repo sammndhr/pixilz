8fb9c1d5e6b2cec39650c7c5568d0c4ea4361f42
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-06-20 17:03:17 -0700

    Update theme icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../styles/App.scss'
 import Navigation from './Navigation.js'
 
-function App() {
-  return (
-    <div className='App container'>
-      <Navigation />
-      <main>
-        <h1>Main</h1>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-        <p>Here is the main body.</p>
-      </main>
-      <footer>Footer</footer>
-    </div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App container'>
+        <Navigation />
+        <main>
+          <h1>Main</h1>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+          <p>Here is the main body.</p>
+        </main>
+        <footer>Footer</footer>
+      </div>
+    )
+  }
 }
-
-export default App
