176ade619b1d8af733019f68d450b1733c69cea9
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-06-25 23:29:55 -0700

    Add download button for canvases

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -7,6 +7,19 @@ export default class Canvas extends Component {
   divRef = React.createRef()
   state = { canvases: [] }
 
+  componentDidMount() {
+    this.createCanvas()
+  }
+
+  componentDidUpdate() {
+    const images = [...this.divRef.current.children]
+    for (let i = 0; i < images.length; i++) {
+      const img = images[i]
+      img.onload = () => {
+        this.drawCanvas(img, i)
+      }
+    }
+  }
   createCanvas = () => {
     const images = [...this.divRef.current.children]
     const canvases = images.map((img, i) => {
@@ -25,29 +38,29 @@ export default class Canvas extends Component {
     this.setState({ canvases })
   }
 
-  componentDidMount() {
-    this.createCanvas()
-  }
-
-  componentDidUpdate() {
-    const images = [...this.divRef.current.children]
-    for (let i = 0; i < images.length; i++) {
-      const img = images[i]
-      img.onload = () => {
-        this.drawCanvas(img, i)
-      }
-    }
-  }
-
   drawCanvas = (img, i) => {
     const canvas = this.canvasRefs[`canvas${i}`]
     const ctx = canvas.getContext('2d')
     ctx.drawImage(img, 0, 0)
   }
 
+  handleClik = () => {
+    for (const key in this.canvasRefs) {
+      if (this.canvasRefs.hasOwnProperty(key)) {
+        const canvas = this.canvasRefs[key]
+        const url = canvas.toDataURL('image/png')
+        const link = document.createElement('a')
+        link.download = `${key}.png`
+        link.href = url
+        link.click()
+      }
+    }
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleClik}>Download</button>
         <div className='canvas-wrapper'>{this.state.canvases}</div>
         <div className='images' ref={this.divRef} style={{ display: 'none' }}>
           <img id='test1' src={test01} alt='test01' />
