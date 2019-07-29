ac8dbc14a69ab778a91542a0818cdd0bc9ad87fe
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-07-28 22:15:04 -0700

    Extract sortFiles to utils/

--- src/components/UploadImages.js
+++ src/components/UploadImages.js
@@ -1,32 +1,12 @@
 import React, { Component, Fragment } from 'react'
 import DataContext from '../context/DataContext'
 import { withRouter } from 'react-router'
-import Button from '../common/Button'
 import Form from '../common/Form'
-
+import sortFiles from '../utils/sortFiles'
 class Main extends Component {
   static contextType = DataContext
 
   state = { dataUrls: [], uploadStatus: false, checked: true }
-
-  sortFilesByName = filesArr => {
-    const reA = /[^a-zA-Z]/g
-    const reN = /[^0-9]/g
-    const sortAlphaNum = (objA, objB) => {
-      const a = objA.name
-      const b = objB.name
-      const aA = a.replace(reA, '')
-      const bA = b.replace(reA, '')
-      if (aA === bA) {
-        const aN = parseInt(a.replace(reN, ''), 10)
-        const bN = parseInt(b.replace(reN, ''), 10)
-        return aN === bN ? 0 : aN > bN ? 1 : -1
-      } else {
-        return aA > bA ? 1 : -1
-      }
-    }
-    return filesArr.sort(sortAlphaNum)
-  }
 
   readFile = file => {
     return new Promise((resolve, reject) => {
@@ -44,15 +24,11 @@ class Main extends Component {
 
   readMultipleFiles = async files => {
     const filesArr = Array.from(files)
-    const filesToProcess = this.state.checked
-      ? this.sortFilesByName(filesArr)
-      : filesArr
-
+    const filesToProcess = this.state.checked ? sortFiles(filesArr) : filesArr
     const promises = filesToProcess.map(async file => {
       const data = await this.readFile(file)
       return data
     })
-
     return Promise.all(promises).then(results => {
       return results
     })
---
+++ src/utils/sortFiles.js
@@ -0,0 +1,20 @@
+const sortFilesByName = filesArr => {
+  const reA = /[^a-zA-Z]/g
+  const reN = /[^0-9]/g
+  const sortAlphaNum = (objA, objB) => {
+    const a = objA.name
+    const b = objB.name
+    const aA = a.replace(reA, '')
+    const bA = b.replace(reA, '')
+    if (aA === bA) {
+      const aN = parseInt(a.replace(reN, ''), 10)
+      const bN = parseInt(b.replace(reN, ''), 10)
+      return aN === bN ? 0 : aN > bN ? 1 : -1
+    } else {
+      return aA > bA ? 1 : -1
+    }
+  }
+  return filesArr.sort(sortAlphaNum)
+}
+
+export default sortFilesByName
