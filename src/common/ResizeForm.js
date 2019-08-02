1a35fa267b42b22a260b88d1d3695c0013b6a5f3
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-08-01 21:21:03 -0700

    Testing react slider

--- src/common/ResizeForm.js
+++ src/common/ResizeForm.js
@@ -1,36 +1,60 @@
-import React, { Fragment, useState, useContext, useEffect } from 'react'
+import React, { Fragment, useState, useContext, useEffect, useRef } from 'react'
 import DataContext from '../context/DataContext'
 
 const Resize = () => {
-  const data = useContext(DataContext)
-  const [value, setValue] = useState(4)
-  const max = 200
-  const [position, setPosition] = useState(value)
-  // const showSliderValue = e => {
-  //   console.log(e.target)
-  //   console.log(rangeSlider.value)
-  //   rangeBullet.value = rangeSlider.value
-  //   var bulletPosition = rangeSlider.value / rangeSlider.max
-  //   rangeBullet.style.left = bulletPosition * 578 + 'px'
-  // }
+  const dimensions = useContext(DataContext).dimensions
+  const [value, setValue] = useState(100)
+  const [{ min, max }, setMinMax] = useState({ min: 100, max: 720 })
+  const [position, setPosition] = useState(value - min)
+  const handleFocus = value => {
+    setValue(value)
+    const position = `${((value - 100) / (max - min)) * 578}px`
+    console.log(position)
+    setPosition(position)
+  }
+  const calculatePosition = value => {
+    const position = `${((value - min) / (max - min)) * 578}px`
+    return position
+  }
   const handleChange = function(event) {
     const value = event.target.value
-
-    console.log(value)
     setValue(value)
-    const position = `${(value / max) * 578}px`
+    const position = calculatePosition(value)
     setPosition(position)
   }
   useEffect(() => {
-    console.log(value)
+    if (position < 100) {
+    }
     setValue(value)
-    console.log(data)
-  }, [value, data])
-  // var rangeSlider = document.getElementById('rs-range-line')
-  // var rangeBullet = document.getElementById('rs-bullet')
-
+    console.log(dimensions)
+    if (!dimensions.minMaxWidth) return
+    const minMaxWidth = dimensions.minMaxWidth
+    const min = minMaxWidth.min
+    const max = minMaxWidth.max
+    setMinMax({ min, max })
+  }, [value, dimensions, position])
+  const handleKeyPress = event => {
+    if (
+      (event.target.value > max || event.target.value < min) &&
+      event.keyCode !== 46 &&
+      event.keyCode !== 8
+    ) {
+      event.preventDefault()
+      setValue(min)
+      setPosition(calculatePosition(min))
+    }
+  }
   return (
-    <div className='container'>
+    <div className='container' style={{ backgroundColor: 'black' }}>
+      <input
+        onKeyDown={handleKeyPress}
+        onKeyUp={handleKeyPress}
+        type='number'
+        onChange={handleChange} //write new onChange
+        value={value} //create new state
+        min={min}
+        max={max}
+      />
       <div className='range-slider'>
         <span id='rs-bullet' className='rs-label' style={{ left: position }}>
           {value}
@@ -39,28 +63,18 @@ const Resize = () => {
           id='rs-range-line'
           className='rs-range'
           type='range'
-          min='0'
-          defaultValue={value}
+          min={min}
+          value={value}
           max={max}
           onChange={handleChange}
         />
       </div>
 
       <div className='box-minmax'>
-        <span>0</span>
-        <span>200</span>
+        <span>{min}</span>
+        <span>{max}</span>
       </div>
     </div>
   )
 }
 export default Resize
-
-/* <input
-      id='typeinp'
-      type='range'
-      min='0'
-      max='15'
-      defaultValue={value}
-      onChange={handleChange}
-      step='1'
-    /> */
