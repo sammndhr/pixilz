c269ed23538194dcdf63988f89e674d0b07d06c7
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-08-08 12:59:18 -0700

    Style main, aside, .canvas-wrapper

--- src/common/ResizeForm.js
+++ src/common/ResizeForm.js
@@ -2,25 +2,130 @@ import React, { Fragment, useState, useContext, useEffect } from 'react'
 import DataContext from '../context/DataContext'
 
 const Resize = () => {
-  const data = useContext(DataContext)
-  const [value, setValue] = useState(4)
-  const handleChange = function(event) {
-    setValue({ value: event.target.value })
+  const dimensions = useContext(DataContext).dimensions
+  const [errors, setErrors] = useState({
+    width: false,
+    height: false
+  })
+  const [w, setWidth] = useState({
+    width: 720,
+    avgWidth: 720,
+    minWidth: 100,
+    maxWidth: 1080
+  })
+
+  const [h, setHeight] = useState({
+    height: 720,
+    avgHeight: 720,
+    minHeight: 100,
+    maxHeight: 5000
+  })
+
+  const validate = ({ width = 0, height = 0 }) => {
+    console.log(width <= 1080 && width >= 100, width)
+    console.log(height <= 3000 && height >= 100, 'height', height)
+    return {
+      width: width <= 1080 && width >= 100,
+      height: height <= 3000 && height >= 100
+    }
+  }
+
+  const handleWidthChange = event => {
+    const error = !validate({ width: event.target.value }).width
+    if (error) {
+      setErrors({
+        ...errors,
+        width: error
+      })
+    } else {
+      setErrors({
+        ...errors,
+        width: false
+      })
+    }
+    setWidth({
+      ...w,
+      width: event.target.value
+    })
+  }
+  const handleHeightChange = event => {
+    const error = !validate({ height: event.target.value }).height
+    if (error) {
+      setErrors({
+        ...errors,
+        height: error
+      })
+    } else {
+      setErrors({
+        ...errors,
+        height: false
+      })
+    }
+    setHeight({
+      ...h,
+      height: event.target.value
+    })
   }
   useEffect(() => {
-    setValue(value)
-    console.log(data)
-  }, [value, data])
+    // if (dimensions.avgWidth) {
+    //   const minMaxWidth = dimensions.minMaxWidth
+    // }
+    console.log(errors)
+  }, [errors])
+  //     setWidth(dimensions.avgWidth)
+  //     setwidth({
+  //       avgWidth: dimensions.avgWidth,
+  //       minWidth: minMaxWidth.min,
+  //       maxWidth: minMaxWidth.max
+  //     })
+  //   }
+  //   if (dimensions.avgHeight) {
+  //     console.log(errors)
+  //     const minMaxHeight = dimensions.minMaxHeight
+  //     setHeight(dimensions.avgHeight)
+  //     setHeight({
+  //       avgHeight: dimensions.avgHeight,
+  //       minHeight: minMaxHeight.min,
+  //       maxHeight: minMaxHeight.max
+  //     })
+  //   }
+  // }, [])
+
   return (
-    <input
-      id='typeinp'
-      type='range'
-      min='0'
-      max='15'
-      defaultValue={value}
-      onChange={handleChange}
-      step='1'
-    />
+    <div className='options'>
+      <fieldset>
+        <legend>Options</legend>
+        <label className='form-check-label' htmlFor='width'>
+          <div>Current Average Width</div>
+          <div>Exact Width</div>
+          <span>{w.minWidth}&gt;</span>
+          <input
+            className={errors.width ? 'error input' : 'input'}
+            min={w.minWidth}
+            max={w.maxWidth}
+            type='number'
+            onChange={handleWidthChange}
+            value={w.width}
+            name='width'
+          />
+          <span>&lt;{1080}</span>
+        </label>
+        <label className='form-check-label' htmlFor='height'>
+          <div> Average Height</div>
+          <span>{h.minHeight}&gt;</span>
+          <input
+            className={errors.height ? 'error input' : 'input'}
+            min={h.minHeight}
+            max={h.maxHeight}
+            type='number'
+            onChange={handleHeightChange}
+            value={h.height}
+            name='height'
+          />
+          <span>&lt;{h.maxHeight}</span>
+        </label>
+      </fieldset>
+    </div>
   )
 }
 export default Resize
