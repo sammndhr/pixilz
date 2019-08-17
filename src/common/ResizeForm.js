badabb571e9b7d0a0ab9c5395eb935166109baf3
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-08-17 12:04:28 -0700

    Fix bugs

--- src/common/ResizeForm.js
+++ src/common/ResizeForm.js
@@ -1,25 +1,38 @@
-import React, { useState, useEffect } from 'react'
-// import DataContext from '../context/DataContext'
+import React, { useState, useEffect, useContext } from 'react'
+import DataContext from '../context/DataContext'
 
 const Resize = () => {
-  // const dimensions = useContext(DataContext).dimensions
+  const data = useContext(DataContext)
   const [errors, setErrors] = useState({
     width: false,
     height: false
   })
   const [w, setWidth] = useState({
-    width: 720,
-    avgWidth: 720,
-    minWidth: 100,
-    maxWidth: 1080
+    min: 0,
+    max: 0,
+    avg: 0
   })
 
   const [h, setHeight] = useState({
-    height: 720,
-    avgHeight: 720,
-    minHeight: 100,
-    maxHeight: 5000
+    min: 0,
+    max: 0,
+    avg: 0
   })
+  useEffect(() => {
+    if (data.dimensions.w && data.dimensions.h) {
+      // console.log(data.dimensions.dimensions)
+      console.log(data.dimensions.w, data.dimensions.h)
+      setWidth(data.dimensions.w)
+      setHeight(data.dimensions.h)
+    }
+
+    // if (data.dimensions.h) {
+    // }
+  }, [data])
+  useEffect(() => {
+    console.log(w.min, h.min)
+    console.log(w.max, h.max)
+  }, [w, h])
 
   const validate = ({ width = 0, height = 0 }) => {
     console.log(width <= 1080 && width >= 100, width)
@@ -29,7 +42,9 @@ const Resize = () => {
       height: height <= 3000 && height >= 100
     }
   }
-
+  const handleChange = event => {
+    console.log(event)
+  }
   const handleWidthChange = event => {
     const error = !validate({ width: event.target.value }).width
     if (error) {
@@ -74,18 +89,18 @@ const Resize = () => {
   //     setWidth(dimensions.avgWidth)
   //     setwidth({
   //       avgWidth: dimensions.avgWidth,
-  //       minWidth: minMaxWidth.min,
-  //       maxWidth: minMaxWidth.max
+  //       min: minMaxWidth.min,
+  //       max: minMaxWidth.max
   //     })
   //   }
   //   if (dimensions.avgHeight) {
   //     console.log(errors)
-  //     const minMaxHeight = dimensions.minMaxHeight
+  //     const minMax = dimensions.minMax
   //     setHeight(dimensions.avgHeight)
   //     setHeight({
   //       avgHeight: dimensions.avgHeight,
-  //       minHeight: minMaxHeight.min,
-  //       maxHeight: minMaxHeight.max
+  //       min: minMax.min,
+  //       max: minMax.max
   //     })
   //   }
   // }, [])
@@ -94,14 +109,36 @@ const Resize = () => {
     <div className='options'>
       <fieldset>
         <legend>Options</legend>
+        <label className='form-check-label' htmlFor='smallest'>
+          <input
+            className='input'
+            type='radio'
+            name='resize'
+            id='smallest'
+            checked={data.scaleDown}
+            onChange={handleChange}
+          />
+          <span>Resize to smallest image {w.min}px</span>
+        </label>
+        <label className='form-check-label' htmlFor='largest'>
+          <input
+            className='input'
+            type='radio'
+            name='resize'
+            id='largest'
+            checked={data.scaleUp}
+            onChange={handleChange}
+          />
+          <span>Resize to largest image {w.max}px</span>
+        </label>
+
         <label className='form-check-label' htmlFor='width'>
-          <div>Current Average Width</div>
           <div>Exact Width</div>
-          <span>{w.minWidth}&gt;</span>
+          <span>{w.min}&gt;</span>
           <input
             className={errors.width ? 'error input' : 'input'}
-            min={w.minWidth}
-            max={w.maxWidth}
+            min={w.min}
+            max={w.max}
             type='number'
             onChange={handleWidthChange}
             value={w.width}
@@ -111,17 +148,17 @@ const Resize = () => {
         </label>
         <label className='form-check-label' htmlFor='height'>
           <div> Average Height</div>
-          <span>{h.minHeight}&gt;</span>
+          <span>{h.min}&gt;</span>
           <input
             className={errors.height ? 'error input' : 'input'}
-            min={h.minHeight}
-            max={h.maxHeight}
+            min={h.min}
+            max={h.max}
             type='number'
             onChange={handleHeightChange}
             value={h.height}
             name='height'
           />
-          <span>&lt;{h.maxHeight}</span>
+          <span>&lt;{h.max}</span>
         </label>
       </fieldset>
     </div>
