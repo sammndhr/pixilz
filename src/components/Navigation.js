1a305d1896ea3b4508b50c33df0c8a4d5cf95982
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2019-09-25 14:08:14 -0700

    Convert Navigation component to use hooks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import sun from '../images/sun.svg'
 import moon from '../images/moon.svg'
 import ThemeContext from '../context/ThemeContext'
 
-export default class Navigation extends Component {
-  static contextType = ThemeContext
+const Navigation = () => {
+  const theme = useContext(ThemeContext)
+  const [scrolled, setscrolled] = useState(false)
+  const [headerClass, setheaderClass] = useState('header')
 
-  state = {
-    scrolled: false
-  }
+  useEffect(() => {
+    setheaderClass(scrolled ? 'header scroll' : 'header')
+  }, [scrolled])
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll)
-  }
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll)
-  }
-
-  handleScroll = () => {
-    if (window.scrollY > 20) {
-      this.setState({ scrolled: true })
-    } else {
-      this.setState({ scrolled: false })
+  useEffect(() => {
+    const handleScroll = () => {
+      setscrolled(window.scrollY > 20 ? true : false)
     }
-  }
+    window.addEventListener('scroll', handleScroll)
+    const cleanup = () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+    return cleanup
+  }, [])
 
-  renderThemeButton = theme => {
+  const renderThemeButton = theme => {
     return theme.dark ? (
       <span>
         <img src={sun} className='theme-icon' alt='Light Mode' />
@@ -37,27 +35,25 @@ export default class Navigation extends Component {
       </span>
     )
   }
-  render() {
-    const { scrolled } = this.state
-    const theme = this.context
 
-    return (
-      <header className={scrolled ? 'header scroll' : 'header'}>
-        <nav className='nav'>
-          <div className='brand'>
-            <Link to='/'>
-              <span className='text'>STITCH 'N' SLICE</span>
-            </Link>
+  return (
+    <header className={headerClass}>
+      <nav className='nav'>
+        <div className='brand'>
+          <Link to='/'>
+            <span className='text'>STITCH 'N' SLICE</span>
+          </Link>
+        </div>
+        <div className='links'>
+          <div className='cta-btn'>
+            <button className='dark-switcher' onClick={theme.toggleTheme}>
+              {renderThemeButton(theme)}
+            </button>
           </div>
-          <div className='links'>
-            <div className='cta-btn'>
-              <button className='dark-switcher' onClick={theme.toggleTheme}>
-                {this.renderThemeButton(theme)}
-              </button>
-            </div>
-          </div>
-        </nav>
-      </header>
-    )
-  }
+        </div>
+      </nav>
+    </header>
+  )
 }
+
+export default Navigation
