2f2c44d222b0c295d4599592d80c1e1a72a70e8e
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-03-04 13:13:03 -0800

    Add react-ga

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 import { DataProvider } from './context/DataContext'
 import { ThemeProvider } from './context/ThemeContext'
 import * as serviceWorker from './serviceWorker'
+
+ReactGA.initialize('UA-121879226-1')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(
   <Router>
